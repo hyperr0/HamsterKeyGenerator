227e9670e1d4cc8f6d54e70642aebee475933f86
Hyper <141940034+hyperr0@users.noreply.github.com>
2024-08-16 12:24:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,14 @@ const gamePromoConfigs = {
         appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
         promoId: '43e35910-c168-4634-ad4f-52fd764a843f'
     },
+    MergeAway: {
+        appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
+        promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4'
+    },
+    TwerkRace: {
+        appToken: "61308365-9d16-4040-8bb0-2f4a4c69074c",
+	    promoId: "61308365-9d16-4040-8bb0-2f4a4c69074c"
+    },
 };
 
 let currentAppConfig = gamePromoConfigs.MyCloneArmy;
