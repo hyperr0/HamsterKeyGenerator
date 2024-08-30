564144dececb534e232e72b51ab6667aad5aa80b
Hyper <141940034+hyperr0@users.noreply.github.com>
2024-08-30 11:41:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,69 @@
 document.addEventListener('DOMContentLoaded', () => {
     const games = {
-        1: {
-            name: 'Riding Extreme 3D',
-            appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-            promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-            timing: 30000, // 30 seconds
-            attempts: 25,
-        },
-        2: {
-            name: 'Chain Cube 2048',
-            appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
-            promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
-            timing: 30000, // 30 seconds
-            attempts: 20,
-        },
-        3: {
-            name: 'My Clone Army',
-            appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-            promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
-            timing: 180000, // 180 seconds
-            attempts: 30,
-        },
-        4: {
-            name: 'Train Miner',
-            appToken: '82647f43-3f87-402d-88dd-09a90025313f',
-            promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-            timing: 30000, // 30 seconds
-            attempts: 15,
-        },
-        5: {
-            name: 'Merge Away',
-            appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
-            promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
-            timing: 30000, // 30 seconds
-            attempts: 25,
-        },
-        6: {
-            name: 'Twerk Race 3D',
-            appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-            promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-            timing: 30000, // 30 seconds
-            attempts: 20,
-        },
-        7: {
-            name: 'Polysphere',
-            appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
-            promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
-            timing: 20000, // 20 seconds
-            attempts: 20,
-        },
-        8: {
-            name: 'Mow and Trim',
-            appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
-            promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
-            timing: 20000, // 20 seconds
-            attempts: 20,
-        },
-        9: {
-            name: 'Mud Racing',
-            appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            timing: 20000, // 20 seconds
-            attempts: 20,
-        },
-        10: {
-            name: 'Cafe Dash',
-            appToken: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-            promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-            timing: 20000, // 20 seconds
-            attempts: 20,
-        },
-        11: {
-            name: 'Zoopolis',
-            appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
-            promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
-            timing: 20000, // 20 seconds
-            attempts: 20,
-        }
-    };
+    1: {
+        name: 'Chain Cube 2048',
+        appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
+        promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
+        interval: 20,
+        eventCount: 3,
+    },
+    2: {
+        name: 'Train Miner',
+        appToken: '82647f43-3f87-402d-88dd-09a90025313f',
+        promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
+        interval: 120,
+        eventCount: 1,
+    },
+    3: {
+        name: 'Merge Away',
+        appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
+        promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
+        interval: 21,
+        eventCount: 7,
+    },
+    4 : {
+        name: 'TwerkRace',
+        appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+        promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+        interval: 20,
+        eventCount: 10,
+    },
+    5: {
+        name: 'Polysphere',
+        appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+        promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+        interval: 20,
+        eventCount: 16,
+    },
+    6: {
+        name: 'Mow and Trim',
+        appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+        promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+        interval: 20,
+        eventCount: 10,
+    },
+    7: {
+        name: 'Cafe Dash',
+        appToken: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
+        promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
+        interval: 20,
+        eventCount: 16,
+    },
+    8: {
+        name: 'Gangs Wars',
+        appToken: 'b6de60a0-e030-48bb-a551-548372493523',
+        promoId: 'b6de60a0-e030-48bb-a551-548372493523',
+        interval: 20,
+        eventCount: 16,
+    },
+    9: {
+        name: 'Zoopolis',
+        appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+        promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+        interval: 20,
+        eventCount: 7,
+    }
+};
 
     const gameOptions = document.querySelectorAll('.game-option');
     const keyCountGroup = document.getElementById('keyCountGroup');
